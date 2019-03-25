45e88feea52c1c07f9b5b9d0f09840d46709a952
gofir <gofir@ebay.com>
2019-03-25 16:47:08 +0200

    progress

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -66,9 +66,61 @@ class DecisionNode:
         
     def add_child(self, node):
         self.children.append(node)
+        
+    def has_children(self):
+        return len(self.children) > 0 and (self.children[0] or self.children[1])
 
 
-def build_tree(data, impurity):
+        
+def avg_of_pairs(lis):
+    if len(lis) < 2:
+        return lis
+    return np.array([(x+y)/2.0 for x,y in zip(lis, lis[1:])])
+
+def find_best_attribute(data, impurity):
+    c = data.shape[1] - 1
+    node_impurity = impurity(data)
+    best = 0
+    attr = 0
+    value = 0
+    left = np.array([])
+    right = np.array([])
+    for i in range(c):
+        thresholds = avg_of_pairs(data[:,i])
+        for threshold in thresholds:
+            left_child = data[data[:,i] <= threshold]
+            right_child = data[data[:, i] > threshold]
+            split = impurity(left_child) + impurity(right_child)
+            gain = node_impurity - split
+            if (gain > best):
+                best = gain
+                left = left_child
+                right = right_child
+                attr = i
+                value = threshold
+    return left,right,value,attr
+
+def get_chi(left, right):
+    chi = 0
+    p0 = left[left[:, -1]<1].shape[0]
+    n0 = left.shape[0] - p0
+    D0 = left.shape[0]
+    p1 = right[right[:,-1]<1].shape[0]
+    n1 = right.shape[0] - p1
+    D1 = right.shape[0]
+    PY0 = (left[left[:,-1]<1].shape[0] + right[right[:,-1]<1].shape[0]) / (left.shape[0] + right.shape[0])
+    PY1 = 1 - PY0
+    E0l = D0 * PY0
+    E1l = D0 * PY1
+    
+    E0r = D1 * PY0
+    E1r = D1 * PY1
+    l = (((p0 - E0l) ** 2)/E0l) + (((n0 - E1l) ** 2) / E1l)
+    r = (((p1 - E0r) ** 2)/E0r) + (((n1 - E1r) ** 2) / E1r)
+    chi = l + r
+    return chi
+
+def build_tree(data, impurity, chi_value=1):
     """
     Build a tree using the given impurity measure and training dataset. 
     You are required to fully grow the tree until all leaves are pure. 
@@ -80,14 +132,44 @@ def build_tree(data, impurity):
 
     Output: the root node of the tree.
     """
-    root = None
+    root = DecisionNode(data=data)
+    
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################
-    pass
+    def add_nodes(node):
+        if not node:
+            return
+        curr_data = node.data
+        if not curr_data.any() or not impurity(data):
+            return
+        left,right,value,feature = find_best_attribute(curr_data, impurity)
+        node.value = value
+        node.feature = feature
+        left_child = None
+        right_child = None
+        should_prune = False
+        chi = 0
+        if left.any():
+            left_child = DecisionNode(data=left)
+        if right.any():
+            right_child = DecisionNode(data=right)
+        if left_child and right_child and chi_value != 1:
+            chi = get_chi(left, right)
+            should_prune = chi < chi_table[chi_value]
+        if not should_prune:
+            add_nodes(left_child)
+            add_nodes(right_child)
+            node.add_child(left_child)
+            node.add_child(right_child)
+        else:
+            print ('Pre-pruned', chi)
+        
+    
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
+    add_nodes(root)
     return root
 
     
@@ -105,9 +187,20 @@ def predict(node, instance):
     """
     pred = None
     ###########################################################################
-    # TODO: Implement the function.                                           #
+    # TODO: Implement the function. 
+    def traverse(node, instance):
+        if not node:
+            return
+        if not node.has_children():
+            return node.data[:,-1][0]
+        feature = node.feature
+        threshold = node.value
+        if instance[feature] <= threshold:
+            return traverse(node.children[0], instance)
+        else:
+            return traverse(node.children[1], instance)
     ###########################################################################
-    pass
+    pred = traverse(node, instance)
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -125,10 +218,16 @@ def calc_accuracy(node, dataset):
     Output: the accuracy of the decision tree on the given dataset (%).
     """
     accuracy = 0.0
+    predictions = 0
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################
-    pass
+    for data in dataset:
+        success = predict(node, data) == data[-1]
+        if success:
+            predictions += 1.0
+    accuracy = predictions / dataset.shape[0]
+    print ('Accurate predictions: {} out of {}'.format(predictions, dataset.shape[0]))
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -147,7 +246,15 @@ def print_tree(node):
     ###########################################################################
     # TODO: Implement the function.                                           #
     ###########################################################################    
-    pass
+    def recursion_print(node, i):
+        if not node:
+            return
+        to_print = '[X{} <= {}]'.format(node.feature, node.value) if node.has_children() else 'leaf: [{}: {}]'.format(node.data[:,-1][0], node.data.shape[0])
+        print ((' ' * i * 2) + to_print)
+        if node.has_children():
+            recursion_print(node.children[0], i+1)
+            recursion_print(node.children[1] or None, i+1)
+    recursion_print(node, 0)
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
